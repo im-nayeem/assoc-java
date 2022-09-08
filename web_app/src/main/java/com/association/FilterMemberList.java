b6eb516e7ffc73c9336a893b98ac721842690bd1
Nayeem Hossain <nayeem.cse.cou@gmail.com>
2022-09-08 14:56:47 +0600

    Apply log-in and initialization filter

--- web_app/src/main/java/com/association/FilterMemberList.java
+++ web_app/src/main/java/com/association/FilterMemberList.java
@@ -17,25 +17,10 @@ import javax.servlet.http.HttpServletResponse;
  *
  * @author Mestu
  */
-@WebServlet(name = "FilterMemberList", urlPatterns = {"/FilterMemberList"})
+@WebServlet(name = "members-filter", urlPatterns = {"/members-filter"})
 public class FilterMemberList extends HttpServlet {
 
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        try ( PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet FilterMemberList</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet FilterMemberList at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
-        }
-    }
+
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
--- web_app/src/main/webapp/members.jsp
+++ web_app/src/main/webapp/members.jsp
@@ -15,7 +15,7 @@
 
 <div class="content">
     <div class="filter-member">
-        <form action="FilterMemberList" method="post">
+        <form action="members-filter" method="post">
             <select name="dept" id="dept" required>
                 <option selected disabled hidden>Select Department</option>
                 <c:forEach var="dept" items="${sessionScope.memberFilterValue.getDeptList()}">
