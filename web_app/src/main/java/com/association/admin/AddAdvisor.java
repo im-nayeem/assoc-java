120e3b20600ccf7cb3767dfcb104656d0e2ab1bd
Nayeem Hossain <nayeem.cse.cou@gmail.com>
2022-09-09 14:18:44 +0600

    Add advisor managing features

---
+++ web_app/src/main/java/com/association/admin/AddAdvisor.java
@@ -0,0 +1,24 @@
+package com.association.admin;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+
+/**
+ * Created on 09-Sep-22
+ *
+ * @author Nayeem
+ */
+@WebServlet(name = "AddAdvisor", value = "/AddAdvisor")
+public class AddAdvisor extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
